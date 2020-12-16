01d1345720d98dc31484c8811610273bd9058e17
forien <raphael.forien@inrae.fr>
2020-12-16 22:55:58 +0100

    contact rate

--- contact_rate.py
+++ contact_rate.py
@@ -28,8 +28,8 @@ infect = np.sum(EI_covid[:,1]*EI_covid[:,2])
 
 # EI_beta = lockdown.product_dist(E_dist, I_dist)
 
-l = 2
-E_dist = lockdown.gamma_dist(incub, 1, 300)
+l = 4
+E_dist = lockdown.gamma_dist(incub/2, 2, 300)
 I_dist = lockdown.gamma_dist(infect/l, l, 300)
 
 EI_gamma = lockdown.product_dist(E_dist, I_dist)
@@ -40,10 +40,10 @@ EI_fix = np.array([[incub, infect, 1]])
 # EI_bimod = np.array([[4, 2, .5], [4, 14, .5]])
 
 # sir_beta = lockdown.SEIR_nonMarkov(10, 1, 1, .1, EI_beta, np.array([[1, 1]]))
-sir_gamma = lockdown.SEIR_nonMarkov(10, 1, 1, .1, EI_gamma, np.array([[1, 1]]))
-sir_markov = lockdown.SEIR_lockdown(10, 1, 1, .1, infect, 1, incub)
-sir_fix = lockdown.SEIR_nonMarkov(10, 1, 1, .1, EI_fix, np.array([[1, 1]]))
-sir_covid = lockdown.SEIR_nonMarkov(10, 1, 1, .1, EI_covid, np.array([[1, 1]]))
+sir_gamma = lockdown.SEIR_nonMarkov(.2, EI_gamma, '2020-03-16', 10)
+sir_markov = lockdown.SEIR_lockdown(.2, infect, incub, '2020-03-16', 10)
+sir_fix = lockdown.SEIR_nonMarkov(.2, EI_fix, '2020-03-16', 10)
+sir_covid = lockdown.SEIR_nonMarkov(.2, EI_covid, '2020-03-16', 10)
 # sir_bimod = lockdown.SEIR_nonMarkov(10, 1, 1, .1, EI_bimod, np.array([[1, 1]]))
 
 S = [sir_markov, sir_fix, sir_covid, sir_gamma]
@@ -60,9 +60,9 @@ for (i, sir) in enumerate(S):
             contact_rates[j] = np.nan
         else:
             contact_rates[j] = sir.contact_rate(r)
-    plt.plot(rho, infect*contact_rates, label = names[i], linewidth = 1.2)
+    plt.plot(rho, (infect)*contact_rates, label = names[i], linewidth = 1.2)
 
-plt.vlines((-.049, .27), 0, 1e2, linestyle = 'dashed', linewidth = 1)
+plt.vlines((-.049, .27, .04), 0, 1e2, linestyle = 'dashed', linewidth = 1)
     
 #plt.vlines((-.06, .3), 0, 7, linewidth = 1)
 #plt.hlines((0,7), -.06, .3, linewidth = 1)
@@ -73,6 +73,6 @@ plt.grid(True)
 plt.ylabel(r'$R_0$')
 plt.xlabel(r'growth rate ($\rho$)')
 plt.xlim((-.06, .3))
-plt.ylim((0, 5.5))
+plt.ylim((0, 7))
 #plt.ylim((1e-3, 2e2))
 plt.tight_layout(True)
